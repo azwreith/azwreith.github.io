020463786ae7328366924146b178a4e1d9c54ed7
Ujjwal Arora <arora.ujjwal@gmail.com>
2015-08-18 21:06:14 +0530

    Move website to GitHub

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+var textarray = [
+    "printf (Hello, World !);",
+    "PRINT Hello, World !;",
+    "std::cout << 'Hello, World !' << std::endl;",
+    "System.out.println('Hello, World !');",
+    "document.write('Hello, World !');",
+    "Print['Hello, World !']",
+    "NSLog(@'Hello, World !');",
+    "echo 'Hello, World !';",
+    "print('Hello, World !')",
+    "WRITE 'Hello, World !'.",
+    "message 'Hello, World !' with style = popup;",
+    "trace('Hello, World !');",
+    "WRITE 'Hello, World !'.",
+    "MSG: .ASCIIZ 'Hello, World !'",
+    "10 PRINT 'Hello, World !'",
+    "<p>Hello, World !</p>",
+    "put 'Hello, World !'",
+    "puts 'Hello, World !'",
+    "PRINT *, 'Hello, World !'"
+];
+
+function RndText() {
+    var rannum = Math.floor(Math.random() * textarray.length);
+    document.getElementById('arraytext').innerHTML = textarray[rannum];
+}
+onload = function() {
+    RndText();
+}
+
+var inter = setInterval(function() {
+    RndText();
+}, 1000);
+
+$(document).ready(function() {
+    $('body').css('opacity', '0').fadeTo(1500, 1, 'swing');
+});
