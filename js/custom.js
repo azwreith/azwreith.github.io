162be75ad59d677cb31768ae80c391c7058cd630
Ujjwal Arora <arora.ujjwal@gmail.com>
2015-11-06 01:51:47 +0530

    Implement Particles

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ var inter = setInterval(function() {
 
 */
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
